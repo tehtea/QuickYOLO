2fb5cb6bccad9bebd1e0e25e050b503defb0dd78
Jing Wei <jingwei.chew@gmail.com>
2021-03-22 02:17:20 +0800

    fix bug in detection demo

--- detection_demo.py
+++ detection_demo.py
@@ -16,14 +16,10 @@ import tensorflow as tf
 from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp
 from yolov3.configs import *
 
-image_path   = "./IMAGES/kite.jpg"
-image_output_path = "./IMAGES/kite_pred.jpg"
-video_path   = "./IMAGES/test.mp4"
+if __name__ == "__main__":
+    image_path   = "./IMAGES/kite.jpg"
+    image_output_path = "./IMAGES/kite_pred.jpg"
 
-yolo = Load_Yolo_model()
-detect_image(yolo, image_path, image_output_path, CLASSES=TRAIN_CLASSES, input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0))
-print(f"sample predictions saved to {image_output_path}")
-#detect_video(yolo, video_path, "", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0))
-#detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
-
-#detect_video_realtime_mp(video_path, "Output.mp4", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0), realtime=False)
+    yolo = Load_Yolo_model()
+    detect_image(yolo, image_path, image_output_path, CLASSES=TRAIN_CLASSES, input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0))
+    print(f"sample predictions saved to {image_output_path}")
