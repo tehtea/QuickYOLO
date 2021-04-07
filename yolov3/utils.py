0e0ac39e1578497e9c3044ee79416753786cb4d8
tehtea <jingwei.chew@gmail.com>
2021-04-07 01:43:31 +0000

    move old postprocess_boxes function to tensorflow

--- yolov3/utils.py
+++ yolov3/utils.py
@@ -292,14 +292,13 @@ def nms(bboxes, iou_threshold, sigma=0.3, method='nms'):
 
 def postprocess_boxes(pred_bbox, original_image, input_size, score_threshold):
     valid_scale=[0, np.inf]
-    pred_bbox = np.array(pred_bbox)
 
     pred_xywh = pred_bbox[:, 0:4]
     pred_conf = pred_bbox[:, 4]
     pred_prob = pred_bbox[:, 5:]
 
     # 1. (x, y, w, h) --> (xmin, ymin, xmax, ymax)
-    pred_coor = np.concatenate([pred_xywh[:, :2] - pred_xywh[:, 2:] * 0.5,
+    pred_coor = tf.concat([pred_xywh[:, :2] - pred_xywh[:, 2:] * 0.5,
                                 pred_xywh[:, :2] + pred_xywh[:, 2:] * 0.5], axis=-1)
     # 2. (xmin, ymin, xmax, ymax) -> (xmin_org, ymin_org, xmax_org, ymax_org)
     org_h, org_w = original_image.shape[:2]
@@ -308,27 +307,36 @@ def postprocess_boxes(pred_bbox, original_image, input_size, score_threshold):
     dw = (input_size - resize_ratio * org_w) / 2
     dh = (input_size - resize_ratio * org_h) / 2
 
-    pred_coor[:, 0::2] = 1.0 * (pred_coor[:, 0::2] - dw) / resize_ratio
-    pred_coor[:, 1::2] = 1.0 * (pred_coor[:, 1::2] - dh) / resize_ratio
+    pred_xmin_xmax = pred_coor[:, 0::2]
+    pred_xmin_xmax = 1.0 * (pred_xmin_xmax - dw) / resize_ratio
+    pred_ymin_ymax = pred_coor[:, 1::2]
+    pred_ymin_ymax = 1.0 * (pred_ymin_ymax - dh) / resize_ratio
+    pred_coor = tf.concat([pred_xmin_xmax[:, 0:1], pred_ymin_ymax[:, 0:1], pred_xmin_xmax[:, 1:2], pred_ymin_ymax[:, 1:2]], axis=-1)
 
     # 3. clip some boxes those are out of range
-    pred_coor = np.concatenate([np.maximum(pred_coor[:, :2], [0, 0]),
-                                np.minimum(pred_coor[:, 2:], [org_w - 1, org_h - 1])], axis=-1)
-    invalid_mask = np.logical_or((pred_coor[:, 0] > pred_coor[:, 2]), (pred_coor[:, 1] > pred_coor[:, 3]))
-    pred_coor[invalid_mask] = 0
+    pred_coor = tf.concat([tf.maximum(pred_coor[:, 0:1], 0), tf.maximum(pred_coor[:, 1:2], 0), tf.minimum(pred_coor[:, 2:3], org_w - 1), tf.minimum(pred_coor[:, 3:4], org_h - 1)], axis=-1)
+    invalid_mask = tf.logical_or(pred_coor[:, 0:1] > pred_coor[:, 2:3], pred_coor[:, 1:2] > pred_coor[:, 3:4])
+    valid_mask = tf.logical_not(invalid_mask)
+    valid_mask = tf.cast(valid_mask, dtype=tf.float32)
+    pred_coor = pred_coor * valid_mask
 
     # 4. discard some invalid boxes
-    bboxes_scale = np.sqrt(np.multiply.reduce(pred_coor[:, 2:4] - pred_coor[:, 0:2], axis=-1))
-    scale_mask = np.logical_and((valid_scale[0] < bboxes_scale), (bboxes_scale < valid_scale[1]))
+    bboxes_scale = tf.sqrt((pred_coor[:, 2:3] - pred_coor[:, 0:1]) * (pred_coor[:, 3:4] - pred_coor[:, 1:2]))
+    scale_mask = tf.logical_and((valid_scale[0] < bboxes_scale), (bboxes_scale < valid_scale[1]))
+    scale_mask = tf.squeeze(scale_mask, axis=-1)
 
     # 5. discard boxes with low scores
-    classes = np.argmax(pred_prob, axis=-1)
-    scores = pred_conf * pred_prob[np.arange(len(pred_coor)), classes]
+    classes = tf.argmax(pred_prob, axis=-1)
+    classes = tf.expand_dims(classes, axis=-1)
+    scores = pred_conf * tf.reduce_max(pred_prob, axis=-1)
+    scores = tf.expand_dims(scores, axis=-1)
+
     score_mask = scores > score_threshold
-    mask = np.logical_and(scale_mask, score_mask)
+    score_mask = tf.squeeze(score_mask, axis=-1)
+    mask = tf.logical_and(scale_mask, score_mask)
     coors, scores, classes = pred_coor[mask], scores[mask], classes[mask]
 
-    return np.concatenate([coors, scores[:, np.newaxis], classes[:, np.newaxis]], axis=-1)
+    return np.concatenate([coors, scores, classes], axis=-1)
 
 
 def detect_image(Yolo, image_path, output_path, input_size=416, show=False, CLASSES=YOLO_COCO_CLASSES, score_threshold=0.3, iou_threshold=0.45, rectangle_colors=''):
